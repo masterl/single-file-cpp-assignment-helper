62492b18d2a680d96a063407bcdfbd183941d189
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2018-10-02 18:00:52 -0700

    Improves startdev.sh by separating code into functions

--- startdev.sh
+++ startdev.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-then
-  echo -e '\nUsage:'
-  echo -e "\\t$0 <path/to/file.cpp>"
-  exit 1
-fi
+function main() {
+  ensure_source_file_was_informed "$@"
+  generate_paths "$@"
 
-readonly SCRIPTS_ROOT=$( cd "$( dirname "$0" )" && pwd )
+  tmux \
+    new-session "$SCRIPTS_ROOT/autocompile.sh $1" \; \
+    split-window -h "$SCRIPTS_ROOT/autotest.sh $EXECUTABLE_PATH" \; \
+    attach
+}
 
-readonly PROJECT_PATH=$( cd "$( dirname "$1" )" && pwd )
+function ensure_source_file_was_informed() {
+  if [ -z "$1" ]
+  then
+    echo -e '\nUsage:'
+    echo -e "\\t$0 <path/to/file.cpp>"
+    exit 1
+  fi
+}
 
-readonly EXECUTABLE_PATH="$PROJECT_PATH/program.bin"
+function generate_paths() {
+  declare -gr "SCRIPTS_ROOT=$( cd "$( dirname "$0" )" && pwd )"
 
-tmux \
-  new-session "$SCRIPTS_ROOT/autocompile.sh $1" \; \
-  split-window -h "$SCRIPTS_ROOT/autotest.sh $EXECUTABLE_PATH" \; \
-  attach
+  declare -gr "PROJECT_PATH=$( cd "$( dirname "$1" )" && pwd )"
+
+  declare -gr EXECUTABLE_PATH="$PROJECT_PATH/program.bin"
+}
+
+main "$@"
